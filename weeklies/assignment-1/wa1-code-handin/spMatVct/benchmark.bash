629c7d34dd4ce7539ba1099839c4cdf38974dd0f
johnm2102 <tln507@alumni.ku.dk>
2025-09-10 21:45:56 +0200

    stuff

--- weeklies/assignment-1/wa1-code-handin/spMatVct/benchmark.bash
+++ weeklies/assignment-1/wa1-code-handin/spMatVct/benchmark.bash
@@ -1,4 +1,4 @@
-futhark dataset --i64-bounds=0:9999 -g [1000000]i64 --f32-bounds=-7.0:7.0 -g [1000000]f32 --i64-bounds=100:100 -g [10000]i64 --f32-bounds=-10.0:10.0 -g [10000]f32 | ./spMVmult-flat -t /dev/stderr -r 10 > /dev/null
+futhark dataset --i64-bounds=0:9999 -g [1000000]i64 --f32-bounds=-7.0:7.0 -g [1000000]f32 --i64-bounds=100:100 -g [10000]i64 --f32-bounds=-10.0:10.0 -g [10000]f32 > data.in 
 
 echo "Generating data (spMVmult-seq)"
 futhark c spMVmult-seq.fut && ./spMVmult-seq < data.in > data.out
