c1418bc107b2f11be9737ac38376eb080b3ae090
johnm2102 <tln507@alumni.ku.dk>
2025-09-22 21:56:58 +0200

    pls

--- weeklies/assignment-2/w2-code-handin/cuda/pbb_kernels.cuh
+++ weeklies/assignment-2/w2-code-handin/cuda/pbb_kernels.cuh
@@ -224,9 +224,11 @@ scanIncBlock(volatile typename OP::RedElTp* ptr, const uint32_t idx) {
     //   max block size = 32^2 = 1024
     typename OP::RedElTp end = OP::remVolatile(ptr[idx]);
     __syncthreads();
-    if (lane == (WARP-1)) { ptr[warpid] = end; }
+    if (lane == (WARP-1)) { 
+        ptr[warpid] = end; 
+    }
+    
     __syncthreads();
-
     // 3. scan again the first warp.
     if (warpid == 0) scanIncWarp<OP>(ptr, idx);
     __syncthreads();
