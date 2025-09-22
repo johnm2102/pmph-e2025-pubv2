5dd10a0f95faee3b316bc4dfe43b305333500c2c
johnm2102 <tln507@alumni.ku.dk>
2025-09-22 17:32:29 +0200

    bash profile

---
+++ weeklies/assignment-2/w2-code-handin/cuda/task3.bash
@@ -0,0 +1,12 @@
+#! /bin/bash 
+
+module load cuda 
+
+nvcc pbb_main.cu -o pbb_main
+
+arrays={10003565 1003565 103565 13565}
+
+for array in ${arrays[@]}; do
+    echo "Running PBB for array size $array with 1024 threads"
+    ./pbb_main $array 1024 >> pbb_results.txt
+done
