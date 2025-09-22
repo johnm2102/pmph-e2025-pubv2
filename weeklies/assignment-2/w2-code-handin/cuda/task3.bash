61121df775cbb0920069eef01f0de72340a03b24
johnm2102 <tln507@alumni.ku.dk>
2025-09-22 19:49:03 +0200

    stuff

--- weeklies/assignment-2/w2-code-handin/cuda/task3.bash
+++ weeklies/assignment-2/w2-code-handin/cuda/task3.bash
@@ -4,7 +4,7 @@ module load cuda
 
 nvcc pbb_main.cu -o pbb_main
 
-arrays={10003565 1003565 103565 13565}
+arrays=(10003565 1003565 103565 13565)
 
 for array in ${arrays[@]}; do
     echo "Running PBB for array size $array with 1024 threads"
