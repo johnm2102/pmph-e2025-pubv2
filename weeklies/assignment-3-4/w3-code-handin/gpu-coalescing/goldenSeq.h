c33b768f4031afa1d5a3a63057561180d4b6bded
johnm2102 <tln507@alumni.ku.dk>
2025-10-06 22:53:28 +0200

    trying things

--- weeklies/assignment-3-4/w3-code-handin/gpu-coalescing/goldenSeq.h
+++ weeklies/assignment-3-4/w3-code-handin/gpu-coalescing/goldenSeq.h
@@ -18,16 +18,7 @@ void goldenSeq(ElTp* A, ElTp* B, const uint32_t num_rows, const uint32_t num_col
     /***     of code changes                        ***/
     /**************************************************/
     ElTp accum, a_el;
-    for(uint64_t i = 0; i < num_rows; i++) {
-        uint64_t ii = i*num_cols;
-        accum = 0.0;
-        for(uint64_t j = 0; j < num_cols; j++) {
-            a_el  = A[ii + j];
-            accum = sqrt(accum) + a_el*a_el;
-            B[ii + j] = accum;
-        }
-    }
-    /*
+    
     #pragma omp parallel for private(a_el, accum)
     for(uint64_t i = 0; i < num_rows; i++) {
         uint64_t ii = i*num_cols;
@@ -37,7 +28,7 @@ void goldenSeq(ElTp* A, ElTp* B, const uint32_t num_rows, const uint32_t num_col
             accum = sqrt(accum) + a_el*a_el;
             B[ii + j] = accum;
         }
-    }*/
+    }
 }
 
 #endif
